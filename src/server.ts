38d65c295d836b545d8ef30f49beff8a8f60d385
Lázaro Simão da Silva <lazarosimao@Macbooks-MacBook-Pro.local>
2020-08-17 16:35:48 -0300

    Garantindo estilo de código no Node js com prettier - https://www.youtube.com/watch?v=bA9dgNMHVMU

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
 
 export class SetupServer extends Server {
-  
   constructor(private port = 3000) {
     super();
   }
@@ -21,13 +20,10 @@ export class SetupServer extends Server {
 
   private setupControllers(): void {
     const forecastController = new ForecastController();
-    this.addControllers([
-      forecastController
-    ]);
+    this.addControllers([forecastController]);
   }
 
   public getApp(): Application {
     return this.app;
   }
-
 }
